6ca3439188da61b359eeb1f4f8993c52f02e2aeb
AkshaY2039 <ced15i031@iiitdm.ac.in>
2018-11-08 15:26:35 +0530

    Zooming Left

--- Assignment-05/5.2 Scaling.py
+++ Assignment-05/5.2 Scaling.py
@@ -16,8 +16,6 @@ purple = (255, 0, 255) # purple color
 
 # select some properties for display
 screenSize = scrWidth, scrHeight = 1024, 512
-originX = int (scrWidth / 2)
-originY = int (scrHeight / 2)
 
 # draw polygon
 def drawPolygon (vertexX, vertexY, numVertex, color) :
@@ -25,8 +23,8 @@ def drawPolygon (vertexX, vertexY, numVertex, color) :
 		pygame.draw.line (display_box, color, [vertexX[i % numVertex], vertexY[i % numVertex]], [vertexX[(i + 1) % numVertex], vertexY[(i + 1) % numVertex]], 1)
 	pygame.display.update ()
 
-# translated
-def translated (vertexX, vertexY, numVertex, color, scaleX, scaleY) :
+# scaled
+def scaled (vertexX, vertexY, numVertex, color, scaleX, scaleY) :
 	for i in range (numVertex) :
 		pygame.draw.line (display_box, color, [vertexX[i % numVertex] * scaleX, vertexY[i % numVertex] * scaleY], [vertexX[(i + 1) % numVertex] * scaleX, vertexY[(i + 1) % numVertex] * scaleY], 1)
 	pygame.display.update ()
@@ -35,15 +33,15 @@ def translated (vertexX, vertexY, numVertex, color, scaleX, scaleY) :
 if __name__ == '__main__' :
 		# get input from command line
 		numVertex = int (input ('Enter the number of vertices for your polygon : ').strip ())
-		print ('The vertices will be modified into', originX, 'x', originY, 'quadrant')
+		print ('The vertices will be modified into', scrWidth, 'x', scrHeight, 'quadrant')
 		vertexX = list ()
 		vertexY = list ()
 		for i in range (numVertex) :
 			print ("Vertex", i)
 			temp = int (input ('X coordinate : ').strip ())
-			vertexX.append (temp % originX + originX)
+			vertexX.append (temp)
 			temp = int (input ('Y coordinate : ').strip ())
-			vertexY.append (originY - temp % originY)
+			vertexY.append (temp)
 		scaleX = int (input ('Enter the translation along X : ').strip ())
 		scaleY = int (input ('Enter the translation along Y : ').strip ())
 
@@ -52,19 +50,14 @@ if __name__ == '__main__' :
 		display_box.fill (backGroundColor) # set background color of the screen
 		pygame.display.update ()
 
-		# draw axes
-		pygame.draw.line (display_box, white, [originX, 0], [originX, scrHeight], 1)
-		pygame.draw.line (display_box, white, [0, originY], [scrWidth, originY], 1)
-		pygame.display.update ()
-
 		# draw the polygon
 		drawPolygon (vertexX, vertexY, numVertex, green)
-		# draw the polygon translated X
-		translated (vertexX, vertexY, numVertex, red, scaleX, 1)
-		# draw the polygon translated Y
-		translated (vertexX, vertexY, numVertex, blue, 1, scaleY)
-		# draw the polygon translated XY
-		translated (vertexX, vertexY, numVertex, purple, scaleX, scaleY)
+		# draw the polygon scaled X
+		scaled (vertexX, vertexY, numVertex, red, scaleX, 1)
+		# draw the polygon scaled Y
+		scaled (vertexX, vertexY, numVertex, blue, 1, scaleY)
+		# draw the polygon scaled XY
+		scaled (vertexX, vertexY, numVertex, purple, scaleX, scaleY)
 
 		# update display
 		pygame.display.update ()
--- Assignment-05/5.3 Rotation.py
+++ Assignment-05/5.3 Rotation.py
@@ -5,18 +5,67 @@
 
 import pygame # importing pygame module for graphics and sound libraries
 import sys # importing sys module for system-specific functions
-
-# defining delay in terms of milliseconds
-delay = 20
+import math # importing math module for mathematical functions
 
 # defining some colors
 backGroundColor = (0, 0, 0) # background color as black
-white = (255, 255, 255) # white color
-red = (255, 0, 0) # red color
 green = (0, 255, 0) # green color
-blue = (0, 0, 255) # blue color
+purple = (255, 0, 255) # purple color
 
 # select some properties for display
-screenSize = scrWidth, scrHeight = 1024, 512
-originX = int (scrWidth / 2)
-originY = int (scrHeight / 2)
+screenSize = scrWidth, scrHeight = 1024, 600
+
+# draw polygon
+def drawPolygon (vertexX, vertexY, numVertex, color) :
+	for i in range (numVertex) :
+		pygame.draw.line (display_box, color,
+								 [vertexX[i % numVertex],
+								 vertexY[i % numVertex]],
+								 [vertexX[(i + 1) % numVertex],
+								 vertexY[(i + 1) % numVertex]], 1)
+	pygame.display.update ()
+
+# rotated
+def rotated (vertexX, vertexY, numVertex, color, angle) :
+	for i in range (numVertex) :
+		x1 = abs (int (float (vertexX[i % numVertex]) * math.cos (angle) - float (vertexY[i % numVertex]) * math.sin (angle)))
+		y1 = abs (int (float (vertexX[i % numVertex]) * math.sin (angle) + float (vertexY[i % numVertex]) * math.cos (angle)))
+		x2 = abs (int (float (vertexX[(i + 1) % numVertex]) * math.cos (angle) - float (vertexY[(i + 1) % numVertex]) * math.sin (angle)))
+		y2 = abs (int (float (vertexX[(i + 1) % numVertex]) * math.sin (angle) + float (vertexY[(i + 1) % numVertex]) * math.cos (angle)))
+		pygame.draw.line (display_box, color, [x1, y1], [x2, y2], 1)
+	pygame.display.update ()
+
+# start here
+if __name__ == '__main__' :
+		# get input from command line
+		numVertex = int (input ('Enter the number of vertices for your polygon : ').strip ())
+		print ('The vertices will be modified into', scrWidth, 'x', scrHeight, 'quadrant')
+		vertexX = list ()
+		vertexY = list ()
+		for i in range (numVertex) :
+			print ("Vertex", i)
+			temp = int (input ('X coordinate : ').strip ())
+			vertexX.append (temp % scrWidth)
+			temp = int (input ('Y coordinate : ').strip ())
+			vertexY.append (temp & scrHeight)
+		alpha = float (input ("Enter the angle of rotation : ").strip ())
+		angle = math.radians (alpha % 360)
+
+		# initialize the display box (window)
+		display_box = pygame.display.set_mode (screenSize) # set size of screen
+		display_box.fill (backGroundColor) # set background color of the screen
+		pygame.display.update ()
+
+		# draw the polygon
+		drawPolygon (vertexX, vertexY, numVertex, green)
+		# draw the polygon rotated alpa
+		rotated (vertexX, vertexY, numVertex, purple, angle)
+
+		# update display
+		pygame.display.update ()
+
+		# wait for user exit
+		while 1 :
+				for event in pygame.event.get () :
+						if event.type == pygame.QUIT :
+								sys.exit ()
